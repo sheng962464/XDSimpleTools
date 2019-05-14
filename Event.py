3b85e8d58ce84c1f8ca0eac7583fe1c25067a51f
sheng962464 <614490648@qq.com>
2019-05-14 18:26:19 +0800

    更新Events

--- Event.py
+++ Event.py
@@ -3,6 +3,20 @@
 
 '''定义应用中用到的所有事件'''
 
+import os
+
+
+# 读取数据
+def DataReading(DataPath, Data, Point_Column_Num, Data_Format):
+    List_Of_File_In_Folder = os.listdir(DataPath)
+    for File in List_Of_File_In_Folder:
+        FilePath = DataPath + '/' + File
+        if (os.path.isdir(FilePath)):
+            if (File[0] == '.'):  # 去除隐藏文件
+                pass
+            else:
+                pass
+
 
 # 阵列数据转散点数据
 def ArrayDataToScatterDate(ArratData):
@@ -16,11 +30,6 @@ def DataSimpling(BeforeData):
 
 # 矩阵合并
 def MatrixMerge(ListOfMatrix):
-    pass
-
-
-# 读取数据
-def DataReading(DataPath, Data):
     pass
 
 
